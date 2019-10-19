193c4464e2340a4a94e03ab6b5e7b5c81209f95a
Jason Gallavin <jasongallavin@gmail.com>
2019-10-19 15:55:48 -0400

    Add shutdown option

---
+++ shutdown-vm.py
@@ -0,0 +1,16 @@
+import sys
+from proxmoxer import ProxmoxAPI
+
+proxmox_host = sys.argv[1]
+proxmox_user = sys.argv[2]
+proxmox_password = sys.argv[3]
+vm_id = sys.argv[4]
+
+proxmox = ProxmoxAPI(proxmox_host, user=proxmox_user, password=proxmox_password, verify_ssl=False)
+
+proxmox.nodes('pve').qemu().post('%s/status/shutdown' % vm_id)
+
+
+
+
+
