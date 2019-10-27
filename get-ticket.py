52d2dc7dc0fb39f83e912e9a31581b52c714a8e5
Jason Gallavin <jasongallavin@gmail.com>
2019-10-27 02:02:27 -0400

    Fix get-ticket

--- get-ticket.py
+++ get-ticket.py
@@ -1,15 +1,19 @@
 import sys
+import json
 from proxmoxer import ProxmoxAPI
 
 proxmox_host = sys.argv[1]
 proxmox_user = sys.argv[2]
 proxmox_password = sys.argv[3]
-vm_id = sys.argv[4]
+proxmox_node = sys.argv[4]
+vm_id = sys.argv[5]
 
 proxmox = ProxmoxAPI(proxmox_host, user=proxmox_user, password=proxmox_password, verify_ssl=False)
 
 
-proxmox.nodes('pve').qemu().post(vm_id + '/vncproxy', websocket=True)
+resp = proxmox.nodes(proxmox_node).qemu().post('%s/vncproxy' % vm_id, websocket=1)
+
+print(json.dumps(resp))
 
 
 
