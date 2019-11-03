ca1aa951e098d6b9ec7f9f867f81be1e0df42aa9
Jason Gallavin <jasongallavin@gmail.com>
2019-11-03 16:25:04 -0500

    Fix issues with string replace

--- get-user-ticket.py
+++ get-user-ticket.py
@@ -24,8 +24,8 @@ url_encoded_ticket = urllib.parse.quote(ticket, safe='');
 with open(configuration_path, 'r') as f:
     content = f.read()
 
-content_new = re.sub('(proxy_set_header Cookie "PVEAuthCookie=)(.*?)(";)', r'\1' + re.escape(url_encoded_ticket) + r'\3', content, flags=re.M)
-
+content_new = re.sub('(proxy_set_header Cookie "PVEAuthCookie=)(.*?)(";)', r'\1[TICKET]\3', content, flags=re.M)
+content_new = content_new.replace('[TICKET]', url_encoded_ticket)
 f = open(configuration_path, "w")
 f.write(content_new)
 f.close()
