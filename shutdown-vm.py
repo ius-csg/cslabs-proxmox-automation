2b400542eb3ebc14c77db9fae4acd5ab16038511
Jason Gallavin <jasongallavin@gmail.com>
2019-11-05 01:57:01 -0500

    Add proxmox node to start, stop, and shutdown.

--- shutdown-vm.py
+++ shutdown-vm.py
@@ -4,11 +4,12 @@ from proxmoxer import ProxmoxAPI
 proxmox_host = sys.argv[1]
 proxmox_user = sys.argv[2]
 proxmox_password = sys.argv[3]
-vm_id = sys.argv[4]
+proxmox_node = sys.argv[4]
+vm_id = sys.argv[5]
 
 proxmox = ProxmoxAPI(proxmox_host, user=proxmox_user, password=proxmox_password, verify_ssl=False)
 
-proxmox.nodes('pve').qemu().post('%s/status/shutdown' % vm_id)
+proxmox.nodes(proxmox_node).qemu().post('%s/status/shutdown' % vm_id)
 
 print("success")
 
